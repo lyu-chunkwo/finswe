74065182c3b34a6962c9579b02146ffd5a20168e
lyu-chunkwo <lyu.chun.kwo@gmail.com>
2024-01-18 17:50:07 +0600

    index page include adaptive is complete

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,22 @@
 $(function () {
-  
+  $('.reviews__items').slick({
+    infinite: false,
+    slideToShow: 1,
+    responsive: [
+      {
+        breakpoint: 531,
+        settings: {
+          arrows: false,
+          dots: true
+        }
+      }
+    ],
+    prevArrow: '<button class="slick-btn slick-prev"><img class="slick-prev-svg" src="images/ico/arrow-prev.svg" alt="prev"></button>',
+    nextArrow: '<button class="slick-btn slick-next"><img class="slick-next-svg" src="images/ico/arrow-next.svg" alt="next"></button>'
+  }); 
+
+
+  $('.mobile-button').on('click', function () {
+    $('.menu__list, .mobile-button').toggleClass('active');
+  });
 });
